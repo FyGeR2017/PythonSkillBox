222c70ab5efd63d75fd234d365765c9c2aabb623
FyGeR2017 <91911137+FyGeR2017@users.noreply.github.com>
2023-10-05 01:29:11 +0500

    Update task3.1.py

--- skillbox/mod2/task3.1.py
+++ skillbox/mod2/task3.1.py
@@ -1,27 +1,10 @@
-a = int(input('Введите число a: '))
-b = int(input('Введите число b: '))
-c = int(input('Введите число c: '))
+a, b, c = map(int, input('введите три числа через пробел: ').split())
 
-if a > b:
-  if b > c:
-    print(b)
-  else:
-    if a > c:
-      print(c)  
-    else:
-      print(a)
+numbers = [a, b, c]
+for n in numbers:
+  if n <= -1000 or n >= 1000:
+    print('число вне диапазона')
+    break
 else:
-  if a > c:
-    print(a)
-  else:
-    if b > c:
-      print(c)
-    else:
-      print(b)
-      
-if a < -1000 or a > 1000:
-  print('Число a вне диапазона')
-elif b < -1000 or b > 1000:  
-  print('Число b вне диапазона') 
-elif c < -1000 or c > 1000:
-  print('Число c вне диапазона')
+  numbers.sort()
+  print(numbers[1])
